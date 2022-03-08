144c44e83b7b9106fd3370917159fbdc30d2caf3
pwrstudio <pwrpntr@gmail.com>
2022-03-08 15:10:29 +0100

    Donation widget styling

--- src/lib/blocks/donationWidget.svelte
+++ src/lib/blocks/donationWidget.svelte
@@ -4,13 +4,6 @@
 	//  DONATION WIDGET
 	//
 	// # # # # # # # # # # # # #
-
-	// *** PROPS
-	export let b = {};
-
-	// let gl = document.createElement('script');
-	// gl.src = 'https://secure.givelively.org/widgets/simple_donation/the-eyebeam-atelier-inc/donate-to-eyebeam.js?show_suggested_amount_buttons=false&show_in_honor_of=false&address_required=false&has_required_custom_question=false';
-	// document.getElementsByTagName('head')[0].appendChild(gl);
 </script>
 
 <svelte:head>
@@ -19,7 +12,6 @@
 </svelte:head>
 
 <div class="donation-widget">
-	<!-- Begin Give Lively Fundraising Widget -->
 	<div id="give-lively-widget" class="gl-simple-donation-widget" />
 </div>
 
@@ -29,26 +21,116 @@
 	.donation-widget {
 		margin-top: $small-margin;
 		height: 600px;
-		background: red;
 		width: 100%;
+
+		@include screen-size('small') {
+			border-right: $border-style;
+			border-left: $border-style;
+		}
 	}
 
-	:global(.gl-widget__frequency-selection-container) {
+	:global(.donation-widget .gl-widget__frequency-selection-container) {
 		height: 200px;
-		background: yellow;
+		border-top: $border-style;
+		// background: yellow;
 	}
 
-	:global(.gl-widget__input-group) {
+	:global(.donation-widget .gl-widget__input-group) {
 		height: 200px;
-		background: green;
+		// background: green;
+		font-size: $font-size-h2 !important;
+		color: var(--foreground-color) !important;
+		padding-top: 75px !important;
+		border: unset !important;
+		border-radius: 0 !important;
+		border-top: $border-style !important;
 	}
 
-	:global(.gl-widget__payment) {
+	:global(.donation-widget .gl-widget__donation-input-field) {
+		font-size: $font-size-h2 !important;
+		font-family: $SERIF_STACK !important;
+		color: var(--foreground-color) !important;
+		outline: none !important;
+		text-align: center;
+		border-radius: 0 !important;
+		width: 50% !important;
+		border-bottom: 1px solid $grey !important;
+		background: transparent !important;
+	}
+
+	:global(.donation-widget .gl-widget__btn--outline-primary) {
+		background: transparent !important;
+		margin-bottom: 0 !important;
+		padding: 0 !important;
+	}
+
+	:global(.donation-widget .gl-widget__currency) {
+		top: 75px !important;
+		color: var(--foreground-color) !important;
+		font-family: $SERIF_STACK !important;
+		background: transparent !important;
+	}
+
+	:global(.donation-widget .gl-widget__payment) {
+		border-top: $border-style;
 		height: 200px;
-		background: blue;
+		margin-top: 0 !important;
+		// background: blue;
 	}
 
-	:global(.gl-widget__frequency-buttons) {
+	:global(.donation-widget .gl-widget__frequency-buttons) {
 		height: 100% !important;
+		text-transform: uppercase;
+		font-family: $SERIF_STACK !important;
+		font-size: $font-size-extra-small !important;
+	}
+
+	:global(.donation-widget .gl-widget__donation-submit-button) {
+		height: 100% !important;
+		font-size: $font-size-h2 !important;
+		font-family: $SERIF_STACK !important;
+		background: transparent !important;
+		text-transform: capitalize;
+		color: var(--foreground-color) !important;
+		transition: unset !important;
+
+		&:hover {
+			color: var(--foreground-color) !important;
+			background: $grey !important;
+		}
+	}
+
+	:global(.donation-widget .gl-widget__btn) {
+		margin-left: 0 !important;
+		margin-right: 0 !important;
+		border-radius: 0 !important;
+		border: 0 !important;
+		color: black !important;
+
+		&:first-child {
+			border-right: $border-style !important;
+		}
+	}
+
+	:global(.donation-widget .gl-widget__btn--outline-primary) {
+		&:focus {
+			background-color: $grey !important;
+			color: black !important;
+		}
+
+		&:hover {
+			background-color: $grey !important;
+			color: black !important;
+		}
+	}
+
+	:global(.donation-widget .gl-widget--selected-btn) {
+		background-color: $grey !important;
+		color: black !important;
+	}
+
+	:global(.donation-widget .gl-widget__btn--outline-primary:hover:not(.gl-widget--selected-btn)) {
+		background: transparent !important;
+		color: var(--foreground-color) !important;
 	}
 </style>
