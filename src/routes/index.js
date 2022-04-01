5d65fb6a79775690ebea5719cc94b8adad81f584
Brent <bb2906@nyu.edu>
2022-04-01 16:53:45 -0400

    homepage updates

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,9 @@ import { loadData } from "$lib/sanity.js"
 
 export const get = async (request) => {
     const programs = await loadData("*[_type == 'program']")
-    const events = await loadData("*[_type == 'event']{..., people[]->{...}}")
+    const events = await loadData("*[_type in ['event', 'press', 'news', 'note', 'videoPost', 'journalPost']]{..., people[]->{...}} | order(_updatedAt desc)")
     const stickers = await loadData("*[_id == 'stickers']{...}[0]")
+
     return {
         body: {
             programs: programs.status === 404 ? 'ERROR' : programs,
