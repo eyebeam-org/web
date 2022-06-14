d93d23fdc328471593c70bd790f47fc9a19e2736
Brent <bb2906@nyu.edu>
2022-06-14 17:55:46 -0400

    fix prerender?

--- src/routes/events/[slug].js
+++ src/routes/events/[slug].js
@@ -1,5 +1,6 @@
 import { loadData } from "$lib/sanity.js"
 
+export const prerender = false;
 export const get = async (request) => {
 	const page = await loadData("*[_type == 'event' && slug.current == $slug]{...,people[]->{...}, internalLinks[]->{...}}[0]", { slug: request.params.slug })
 	return {
