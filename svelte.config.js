c4a8946ebce1cff2fc53ff1b240a3031c7cb33e7
Brent <bb2906@nyu.edu>
2022-07-12 19:29:59 -0400

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
+import netlify-adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +11,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			default: false
+			default: true
 		}
 	}
 };
