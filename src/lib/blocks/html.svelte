c26f299a7f1e8701976f9ea7ea1e980611335933
Brent <bb2906@nyu.edu>
2022-07-12 15:25:36 -0400

    fix

--- src/lib/blocks/html.svelte
+++ src/lib/blocks/html.svelte
@@ -8,12 +8,13 @@
 
 	// *** PROPS
 	export let b = {};
+	//FIXME: monkeypatch for old wordpress imports
 </script>
 
 {#if b.children && b.children.length > 0}
 	<div class="html-content">
 		{#each b.children as c}
-			{@html c.text}
+{@html c.text.replace(/<\/strong>/g, "</strong><br/>").replace(/<\/b>/g, "</b><br/>").replace(/<strong>/g, "<br/><strong>").replace(/<b>/g, "<br/><b>").replace(/<\/span>/g, "</span><br/>")}
 		{/each}
 	</div>
 {/if}
