55f7218f8319e8086fad411dd10760087b952c17
Brent <bb2906@nyu.edu>
2022-07-12 15:26:41 -0400

    auto-format imported html

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +10,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		prerender: {
-			default: false
+			default: true
 		}
 	}
 };
